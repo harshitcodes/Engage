556a12294b1bfae917313f538ebf71980a5645bb
harshitcodes <harshit.bvcoe@gmail.com>
2015-12-31 22:30:02 +0530

    User Signup Done

--- proto/settings.py
+++ proto/settings.py
@@ -123,6 +123,6 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'harshit.bvcoe'
-EMAIL_HOST_PASSWORD = 'fjfmwzecuqdaayjj'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
